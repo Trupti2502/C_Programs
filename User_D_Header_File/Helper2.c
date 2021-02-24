8a071922f2efb464baf682bcbe1f4b0aa706ae90
Trupti2502 <78984702+Trupti2502@users.noreply.github.com>
2021-02-24 21:51:33 +0530

    File which contains defination of functions whos prototypes inside header file

---
+++ User_D_Header_File/Helper2.c
@@ -0,0 +1,23 @@
+int Maximum(int iValue1,int iValue2)
+{
+	if(iValue1>iValue2)
+	{
+		return iValue1;
+	}
+	else
+	{
+		return iValue2;
+	}
+}
+
+int Minimum(int iValue1,int iValue2)
+{
+	if(iValue1<iValue2)
+	{
+		return iValue1;
+	}
+	else
+	{
+		return iValue2;
+	}
+}
