833570792768d2cca514ac45de67450c643d6ac9
Trupti2502 <78984702+Trupti2502@users.noreply.github.com>
2021-03-01 21:36:39 +0530

    program to print 5 times hello on screen

---
+++ Assignment1_2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+void Display()
+{
+	int i=0;
+	for(i=1;i<=5;i++)
+	{
+		printf("Marvellous\n");
+	}
+}
+int main()
+{
+	Display();
+	return 0;
+}
+
+/*OUTPUT:-
+user@user-Lenovo-G50-80:~/Desktop/LB$ gcc Assignment1_2.c -o myexe
+user@user-Lenovo-G50-80:~/Desktop/LB$ ./myexe
+Marvellous
+Marvellous
+Marvellous
+Marvellous
+Marvellous
+*/
