348a784f82508326e528c2547c0804d08eaa4c4e
Trupti2502 <78984702+Trupti2502@users.noreply.github.com>
2021-03-17 08:59:35 +0530

    . Accept number from user and display below pattern.

---
+++ Assignment11_4.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+void Pattern(int iRow, int iCol)
+{
+        int iCnt=0,jCnt=0;
+	
+        for(iCnt=1;iCnt<=iRow;iCnt++)
+        {
+                for(jCnt=1;jCnt<=iCol;jCnt++)
+                {
+                        if((jCnt%2)==0)
+			{
+				printf("#\t");
+			}
+			else
+			{
+				printf("*\t");
+			}
+                }
+                printf("\n");
+      }
+}
+int main()
+{
+        int iValue1 = 0, iValue2 = 0;
+        printf("Enter number of rows and columns:\n");
+        scanf("%d %d",&iValue1, &iValue2);
+        Pattern(iValue1, iValue2);
+        return 0;
+}
+
+
+/*
+ * Enter number of rows and columns:
+5
+7
+*	#	*	#	*	#	*
+*	#	*	#	*	#	*
+*	#	*	#	*	#	*
+*	#	*	#	*	#	*
+*	#	*	#	*	#	*
+user@user-Lenovo-G50-80:~/Desktop/LB/Assignments/Assignmen*/
