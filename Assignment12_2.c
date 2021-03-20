734a2677c707c97c9915df85c37bd2796d2320f7
Trupti2502 <78984702+Trupti2502@users.noreply.github.com>
2021-03-20 11:11:51 +0530

    Accept number of rows and number of columns from user and display below pattern.

---
+++ Assignment12_2.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+void Pattern(int iRow, int iCol)
+{
+	int iCnt=0,jCnt=0;
+	char ch='\0';
+	for(iCnt=1;iCnt<=iRow;iCnt++)
+	{
+		if((iCnt%2)==0)
+		{
+			ch='a';
+		}
+		else
+		{
+			ch='A';
+		}
+		for(jCnt=1;jCnt<=iCol;jCnt++,ch++)
+		{
+			printf("%c\t",ch);
+		}
+		printf("\n");
+	}
+}
+int main()
+{
+	system("clear");
+	int iValue1 = 0, iValue2 = 0;
+	printf("Enter number of rows and columns:\n");
+	scanf("%d %d",&iValue1, &iValue2);
+	Pattern(iValue1, iValue2);
+	return 0;
+}
+
+
+/*
+ Enter number of rows and columns:
+4
+4
+A	B	C	D	
+a	b	c	d	
+A	B	C	D	
+a	b	c	d	
+user@user-Lenovo-G50-80:~/Desktop/LB/Assignments/Assignment 12$ vim Assignment12_2.c
+*/
