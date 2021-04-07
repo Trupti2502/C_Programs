f8c615c9018aaa29f1b82d44804b401afb5e55da
Trupti2502 <78984702+Trupti2502@users.noreply.github.com>
2021-04-07 10:31:36 +0530

    Accept number of rows and number of columns from user and display below pattern.

---
+++ Assignment15_4.c
@@ -0,0 +1,53 @@
+
+#include<stdio.h>
+void Pattern(int iRow, int iCol)
+{
+        int i=0,j=0;
+        if(iRow!=iCol)
+        {
+                printf("Number of rows and columns should same\n");
+                return;
+        }
+        for(i=1;i<=iRow;i++)
+        {
+                printf("\n");
+                for(j=1;j<=iCol;j++)
+                {
+
+                        if((i==1)||(j==1)||(i==iRow)||(j==iCol)||(i==j))
+                        {
+                                printf("*\t");
+                        }
+			else
+			{
+				printf("\t");
+			}	
+		}
+                printf("\n");
+        }
+}
+int main()
+{
+        system("clear");
+        int iValue1 = 0, iValue2 = 0;
+        printf("Enter number of rows and columns:");
+        scanf("%d %d",&iValue1, &iValue2);
+        Pattern(iValue1, iValue2);
+        return 0;
+}
+
+/*output:-
+ *Enter number of rows and columns:5
+5
+
+*	*	*	*	*	
+
+*	*			*	
+
+*		*		*	
+
+*			*	*	
+
+*	*	*	*	*	
+user@user-Lenovo-G50-80:~/Desktop/LB/Assignments/Assignment 15$ vim Assignment15_4.c
+*/
