8c13a9196cce7846a3ace85e896076e92c922043
Trupti2502 <78984702+Trupti2502@users.noreply.github.com>
2021-04-07 10:28:10 +0530

    Accept number of rows and number of columns from user and display below pattern.

---
+++ Assignment14_3.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+void Pattern(int iRow, int iCol)
+{
+        int i=0,j=0;
+        if(iRow!=iCol)
+        {
+                printf("Number of rows and columns should same\n");
+                return;
+        }
+        for(i=1;i<=iRow;i++)
+        {
+                printf("\n");
+                for(j=1;j<=iCol;j++)
+                {
+
+                        if(i==j)
+                        {
+                                printf("$\t");
+                        }
+                        else
+                        {
+                                printf("*\t");
+                        }
+                }
+                printf("\n");
+        }
+}
+int main()
+{
+        system("clear");
+        int iValue1 = 0, iValue2 = 0;
+        printf("Enter number of rows and columns:");
+        scanf("%d %d",&iValue1, &iValue2);
+        Pattern(iValue1, iValue2);
+        return 0;
+}
+
+/*OUTPUT:-
+ * Enter number of rows and columns:4
+4
+
+$	*	*	*
+
+*	$	*	*
+
+*	*	$	*
+
+*	*	*	$
+user@user-Lenovo-G50-80:~/Desktop/LB/Assignments/Assignment 14$
+
+*/
