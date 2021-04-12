009e5d0b3997a3387917cba959253e052b94e5d3
Trupti2502 <78984702+Trupti2502@users.noreply.github.com>
2021-04-12 18:36:59 +0530

    Add files via upload

---
+++ Assignmnet17_1.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdlib.h>
+int Difference(int Arr[], int iLength)
+{
+	int i=0,sum1=0,sum2=0;
+	for(i=0;i<iLength;i++)
+	{
+		if(Arr[i]%2==0)
+		{
+		    sum2=sum2+Arr[i];
+            	sum1=sum1+Arr[i];
+		}
+		else
+		{
+                        sum2=sum2+Arr[i];
+		}
+	}
+	return (sum2-sum1);
+}
+int main()
+{
+	int iSize = 0,iRet = 0,iCnt = 0;
+	int *p = NULL;
+	printf("Enter number of elements:");
+	scanf("%d",&iSize);
+	p = (int*)malloc(iSize*(sizeof(int)));
+	if(p == NULL)
+	{
+		printf("Unable to allocate memory");
+		return -1;
+	}
+	printf("Enter %d elements\n",iSize);
+	for(iCnt = 0;iCnt<iSize; iCnt++)
+	{
+		printf("Enter %d element :",iCnt+1);
+		scanf("%d",&p[iCnt]);
+	}
+	iRet = Difference(p, iSize);
+	printf("Result is %d\n\n",iRet);
+	free(p);
+	return 0;
+}
+/*output:-
+ * Enter number of elements:6
+Enter 6 elements
+Enter 1 element :85
+Enter 2 element :3
+Enter 3 element :88
+Enter 4 element :93
+Enter 5 element :66
+Enter 6 element :80
+Result is -53
+*/
